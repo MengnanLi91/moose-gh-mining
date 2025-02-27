5100b5cd1ed1b563f34ed6b1d894935565294082
Mengnan Li <mengnanli91@gmail.com>
2025-02-27 10:55:53 -0700

    Github chatbot test

--- recommend_llamaIndex_github.py
+++ recommend_llamaIndex_github.py
@@ -2,6 +2,7 @@ from sentence_transformers import SentenceTransformer
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core import StorageContext, load_index_from_storage, QueryBundle, Settings
 from llama_index.vector_stores.hnswlib import HnswlibVectorStore
+from pathlib import Path
 
 
 def load_model(model_name):
