d282b49ca6e47d14a31abe1928168eae3025f9ef
Mengnan Li <mengnanli91@gmail.com>
2025-02-27 10:30:07 -0700

    Remove unused packages

--- script.py
+++ script.py
@@ -1,9 +1,5 @@
-from numbers import Real
-from pathlib import Path
-import os
 from sentence_transformers import SentenceTransformer
 from llama_index.core.retrievers import VectorIndexRetriever
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import StorageContext, load_index_from_storage, QueryBundle, Settings
 from llama_index.vector_stores.hnswlib import HnswlibVectorStore
 
